fccc12383e2fe3c2e976429969c11f7bd9bb0e96
joseluizduarte <jlduartejr@live.com>
2022-02-01 01:33:28 -0400

    Implementação do giro das cartas

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -13,13 +13,31 @@ function startGame(){
     cards = cards.sort(comparador);
     cards.forEach(
         (element) => {
-            pageMain.innerHTML += `<div class='pageMain__card'><img class='pageMain__card__image' src='assets/images/${element}.gif' alt=''></div>`
+            pageMain.innerHTML += `\
+            <div class='card' onclick='girar(this)'>\
+                <div class='face frontFace'>\
+                    <img class='pageMain__card__image' src='assets/images/front.png' alt=''>\
+                </div>\
+                <div class='face backFace'>\
+                    <img class='pageMain__card__image' src='assets/images/${element}.gif' alt=''>\
+                </div>\
+            </div>\
+            `
         }
     )
 }
 
 function comparador() { 
 	return Math.random() - 0.5; 
+}
+
+function girar(card) {
+    
+    let frontFace = card.querySelector(".frontFace");
+    frontFace.classList.toggle("frontFace--hidden");
+    let backFace = card.querySelector(".backFace");
+    backFace.classList.toggle("backFace--visible");
+    
 }
 
 startGame();
