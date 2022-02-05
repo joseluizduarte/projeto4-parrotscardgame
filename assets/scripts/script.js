41bd7c1b7e5e2a47c77f86fd6693c3654219ec1f
joseluizduarte <jlduartejr@live.com>
2022-02-05 02:58:06 -0400

    Refatoração e implementação do fim de jogo

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,43 +1,122 @@
-let gameOptions = [4,6,8,10,12,14];
+let contadorJogada = 0;
+let carta1;
+let carta2;
+let cartasEscondidas;
 
-function startGame(){
-    let numberOfCards;
+function perguntarNumeroDeCartas(){
+    let opcoesCartas = [4,6,8,10,12,14];
+    let numeroCartas;
     do {
-        numberOfCards = parseInt(prompt("Quantas cartas você deseja?"));
-    } while(!gameOptions.includes(numberOfCards));
-    let pageMain = document.querySelector(".pageMain");
+        numeroCartas = parseInt(prompt("Quantas cartas você deseja?"));
+    } while(!opcoesCartas.includes(numeroCartas));
+    return numeroCartas;
+}
+
+function sortearImagens(numeroImagens) {
     let gifs = ['bobrossparrot','explodyparrot','fiestaparrot','metalparrot','revertitparrot','tripletsparrot','unicornparrot'];
     gifs.sort(comparador);
-    gifs = gifs.slice(0,numberOfCards/2);
-    let cards = gifs.concat(gifs);
-    cards = cards.sort(comparador);
-    cards.forEach(
-        (element) => {
-            pageMain.innerHTML += `\
-            <div class='card' onclick='girar(this)'>\
-                <div class='face frontFace'>\
-                    <img class='pageMain__card__image' src='assets/images/front.png' alt=''>\
-                </div>\
-                <div class='face backFace'>\
-                    <img class='pageMain__card__image' src='assets/images/${element}.gif' alt=''>\
-                </div>\
-            </div>\
-            `
-        }
-    )
+    let imagens = gifs.slice(0,numeroImagens);
+    return imagens
+}
+
+function embaralharCartas(imagens) {
+    let cartas = [];
+    imagens.forEach((imagem)=>{
+        cartas.push(`${imagem}1`);
+        cartas.push(`${imagem}2`);
+    });
+    cartas = cartas.sort(comparador);
+    return cartas;
+}
+
+function distribuirCarta(carta) {
+    let nomeImagem = carta.substring(0,carta.length-1);
+    let pageMain = document.querySelector(".pageMain");
+    pageMain.innerHTML += `<div class='card' id='${carta}'>
+                                <div class='face frontFace'>\
+                                    <img class='pageMain__card__image' src='assets/images/front.png' alt=''>
+                                </div>
+                                <div class='face backFace'>
+                                    <img class='pageMain__card__image' src='assets/images/${nomeImagem}.gif' alt=''>\
+                                </div>
+                            </div>`
+}
+
+function adicionarEventListener(carta) {
+    let elementoCarta = document.querySelector(`#${carta}`);
+    elementoCarta.addEventListener("click", fazerJogada);
+}
+
+function removerEventListener(carta) {
+    let elementoCarta = document.querySelector(`#${carta}`);
+    elementoCarta.removeEventListener("click", fazerJogada);
+}
+
+function girarCarta(elementoCarta) {
+    let frontFace = elementoCarta.querySelector(".frontFace");
+    frontFace.classList.toggle("frontFace--hidden");
+    let backFace = elementoCarta.querySelector(".backFace");
+    backFace.classList.toggle("backFace--visible");    
+}
+
+function removerCarta(cartaID) {
+    return cartasEscondidas.filter((element)=>{return element!==cartaID});
+}
+
+function esconderCartas(){
+    let elementoCarta1 = document.querySelector(`#${carta1}`);
+    let elementoCarta2 = document.querySelector(`#${carta2}`);
+    girarCarta(elementoCarta1);
+    girarCarta(elementoCarta2);
+    carta1 = '';
+    carta2 = '';
+    cartasEscondidas.forEach(adicionarEventListener);
+}
+
+function compararCartas(){
+    carta1Imagem = carta1.substring(0, carta1.length-1);
+    carta2Imagem = carta2.substring(0, carta2.length-1);
+    if (carta1Imagem===carta2Imagem) {
+        cartasEscondidas = removerCarta(carta1);
+        cartasEscondidas = removerCarta(carta2);
+        cartasEscondidas.forEach(adicionarEventListener);
+    } else {
+        setTimeout(esconderCartas, 1000);
+    }
+}
+
+function verificarFimDeJogo() {
+    if (cartasEscondidas.length===0) {
+        alert(`Você ganhou em ${contadorJogada} jogadas`);
+    }
+}
+
+function fazerJogada(event){
+    contadorJogada++;
+    let elementoCarta = event.currentTarget;
+    girarCarta(elementoCarta);
+    if (contadorJogada%2!==0){
+        elementoCarta.removeEventListener("click", fazerJogada);
+        carta1 = elementoCarta.id;
+    } else {
+        cartasEscondidas.forEach(removerEventListener);
+        carta2 = elementoCarta.id;
+        compararCartas();
+        verificarFimDeJogo();
+    }
+}
+
+function startGame(){
+    // let numeroCartas = perguntarNumeroDeCartas();
+    let numeroCartas = 4;
+    let imagens = sortearImagens(numeroCartas/2);
+    cartasEscondidas = embaralharCartas(imagens);
+    cartasEscondidas.forEach(distribuirCarta);
+    cartasEscondidas.forEach(adicionarEventListener);
 }
 
 function comparador() { 
 	return Math.random() - 0.5; 
-}
-
-function girar(card) {
-    
-    let frontFace = card.querySelector(".frontFace");
-    frontFace.classList.toggle("frontFace--hidden");
-    let backFace = card.querySelector(".backFace");
-    backFace.classList.toggle("backFace--visible");
-    
 }
 
 startGame();
